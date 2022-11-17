8b54035fffbe0b876509078671d4cad55a81b8ad
wSpaceByte <107953410+wSpaceByte@users.noreply.github.com>
2022-11-16 20:38:25 -0800

    Finished JS logic; App Complete

--- js/main.js
+++ js/main.js
@@ -1,28 +1,44 @@
 class CheckSixMonths {
-  constructor(currentDE, lowDE, highDE, rebuyDE) {
+  constructor(currentDE, lowDE, highDE, rebuyDE, dataField) {
     this.current = currentDE
     this.low = lowDE
     this.high = highDE
     this.rebuy = rebuyDE
+    this.dataField = dataField
     this.averageUsage = []
     this.monthsArray = [1, 2, 3, 4, 5, 6]
     this.getAvg()
-    //this.getCalc()
+    this.getCalc()
   }
   getAvg() {
     this.averageUsage.push ((this.low + this.high)/2)
-    document.querySelector('#average').innerText = this.averageUsage
+    document.querySelector('#average').innerText = `About ${this.averageUsage}`
   }
   getCalc() {
-    this.monthsArray.forEach ( el => {
-      if (this.crrent < this.averageUsage) {
-        let result = (this.current - this.averageUsage) + rebuyDE.innerText
+    this.monthsArray.forEach ( (el, i) => {
+      if (this.current <= this.averageUsage) {
+        let result = (this.current - this.averageUsage) + this.rebuy
         this.current = result 
+
+        if(this.monthsArray[i] == 1) {
+          this.dataField[i].innerText = `This Month: (Place Order) Current stock will be ${this.current}`
+        } else if (this.monthsArray[i] == 2) {
+          this.dataField[i].innerText = `Next Month: (Place Order) Current stock will be ${this.current}`
+        } else {
+          this.dataField[i].innerText = `Month ${i + 1}: (Place Order) Current stock will be ${this.current}`
+        }
       } else {
-          let result = this.current - this.averageUsage
-          this.current = result
-          //Show `After month #${el} stock will be @ ${this.current}
+        let result = this.current - this.averageUsage
+        this.current = result
+
+        if(this.monthsArray[i] == 1) {
+          this.dataField[i].innerText = `This Month: Current stock is ${this.current}`
+        } else if (this.monthsArray[i] == 2) {
+          this.dataField[i].innerText = `Next Month: Current stock is ${this.current}`
+        } else {
+          this.dataField[i].innerText = `Month ${i + 1}: Current stock is ${this.current}`
       }
+    }
     })
   }
 }
@@ -34,7 +50,7 @@ function lightningProcure() {
   let lowDE = Number(document.querySelector('#lowInput').value)
   let highDE = Number(document.querySelector('#highInput').value)
   let rebuyDE = Number(document.querySelector('#rebuyInput').value)
+  let dataField = document.querySelectorAll('.data')
 
-  let partNumber = new CheckSixMonths (currentDE, lowDE, highDE, rebuyDE)
-  console.log(partNumber)
+  let partNumber = new CheckSixMonths (currentDE, lowDE, highDE, rebuyDE, dataField)
 }
